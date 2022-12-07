fb64f7d41e2f2a9bfc4c00e5ec98258b2d343cca
aimara-marrero <aimara.bmarrero@gmail.com>
2022-12-07 15:46:23 +0000

    FOOTER
    - Inicio footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,21 @@
 <template>
-  <LoginComponent/>
+  <div>
+    <LoginComponent/>
+  <FooterComponent/> 
+  </div>
+  
 </template>
 
 <script>
   import LoginComponent from '../components/LoginComponent.vue'
+  import FooterComponent from '@/components/FooterComponent.vue';
 
   export default {
     name: 'Home',
 
     components: {
       LoginComponent,
+      FooterComponent
     },
   }
 </script>
