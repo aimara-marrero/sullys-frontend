c7bedcc55d438fb27a33fa97e79a3ff6ed6961dc
Jose Antonio <jaher.front@gmail.com>
2022-12-09 01:11:08 +0000

    Some adjusts in confirm delete

--- src/components/PupilsComponent.vue
+++ src/components/PupilsComponent.vue
@@ -45,6 +45,8 @@
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
                     <v-text-field v-model="editedItem.phone" label="Teléfono"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field v-model="editedItem.address" label="Dirección"></v-text-field>
                   </v-col>
                 </v-row>
@@ -62,13 +64,13 @@
             </v-card-actions>
           </v-card>
         </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
+        <v-dialog v-model="dialogDelete" max-width="800px">
           <v-card>
-            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-title class="text-h5">¿Estás seguro de que quieres ELIMINAR este padre del sistema?</v-card-title>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancelar</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">ELIMINAR</v-btn>
               <v-spacer></v-spacer>
             </v-card-actions>
           </v-card>
